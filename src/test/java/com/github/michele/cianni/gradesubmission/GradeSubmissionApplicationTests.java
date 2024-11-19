04dc4a6fe5d9be1b6e5a7ab0a2d57a62eaee55ac
Michele Cianni <michele.cianni@studenti.unicam.it>
2024-11-19 05:05:32 +0100

    Init Project

---
+++ src/test/java/com/github/michele/cianni/gradesubmission/GradeSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.michele.cianni.gradesubmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeSubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
