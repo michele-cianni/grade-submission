b5d2f057500a2118fee770a1b5c346bcfb2ba2ec
Michele Cianni <michele.cianni@studenti.unicam.it>
2024-11-19 10:00:38 +0100

    Add Stubs On Repositories

--- src/main/java/com/github/michele/cianni/gradesubmission/GradeSubmissionApplication.java
+++ src/main/java/com/github/michele/cianni/gradesubmission/GradeSubmissionApplication.java
@@ -1,13 +1,47 @@
 package com.github.michele.cianni.gradesubmission;
 
+import java.time.LocalDate;
+import java.util.stream.Stream;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.github.michele.cianni.gradesubmission.entity.Course;
+import com.github.michele.cianni.gradesubmission.entity.Student;
+import com.github.michele.cianni.gradesubmission.repository.CourseRepository;
+import com.github.michele.cianni.gradesubmission.repository.StudentRepository;
+
+import lombok.AllArgsConstructor;
+@AllArgsConstructor
 @SpringBootApplication
-public class GradeSubmissionApplication {
+public class GradeSubmissionApplication implements CommandLineRunner{
+
+	private final StudentRepository studentRepository;
+
+	private final CourseRepository courseRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GradeSubmissionApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Stream.of(
+			new Student("Harry Potter", LocalDate.parse(("1980-07-31"))),
+            new Student("Ron Weasley", LocalDate.parse(("1980-03-01"))),
+            new Student("Hermione Granger", LocalDate.parse(("1979-09-19"))),
+            new Student("Neville Longbottom", LocalDate.parse(("1980-07-30"))) 
+		).forEach(studentRepository::save);
+
+		Stream.of(
+			new Course("Charms", "CH104", "In this class, you will learn spells concerned with giving an object new and unexpected properties."),
+            new Course("Defence Against the Dark Arts", "DADA", "In this class, you will learn defensive techniques against the dark arts."),
+            new Course("Herbology", "HB311", "In this class, you will learn the study of magical plants and how to take care of, utilise and combat them."),
+            new Course("History of Magic", "HIS393", "In this class, you will learn about significant events in wizarding history."),
+            new Course("Potions", "POT102", "In this class, you will learn correct mixing and stirring of ingredients to create mixtures with magical effects."),
+            new Course("Transfiguration", "TR442", "In this class, you will learn the art of changing the form or appearance of an object.")
+		).forEach(courseRepository::save);
 	}
 
 }
